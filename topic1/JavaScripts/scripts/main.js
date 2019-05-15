61947a3a55d14f85ad9a89ad9617020793a6cb69
BraianQ <50342216+BraianQ@users.noreply.github.com>
2019-05-15 20:53:56 -0300

    beginning excersice 3

--- topic1/JavaScripts/scripts/main.js
+++ topic1/JavaScripts/scripts/main.js
@@ -1,6 +1,46 @@
-function ocultar() {
-    document.getElementsByClassName('hidden').style.visibility = "hidden";
-    alert(2);
+function loadpage(){
+function hide() {
+    document.querySelector(".hidden").classList.add("hide");
 }
 
-ocultar();
+/*hide();*/
+/*-----------------------------------------------------------------------------------------------------*/
+/*
+function newalert(){
+    const xhr = new XMLHttpRequest();
+    const url = 'http://api.icndb.com/jokes/random';
+    let box;
+    xhr.open('GET', url);
+    xhr.responseType = 'json';
+    xhr.send('');
+    xhr.onload = function (){
+        if (this.status == 200){
+            box = xhr.response;
+            document.querySelector(".hidden").innerHTML = box.value.joke;
+        }else{
+            document.querySelector(".hidden").classList.add("error");
+
+        }
+    }
+}*/
+
+let nodoBoton = document.querySelector('#button');
+nodoBoton.addEventListener('click',newalert);
+
+/*---------------------------------------------------------------------------------------------------*/
+
+let config = {url : 'http://api.icndb.com/jokes/random' , responseType : 'json' , method : 'GET'};
+
+function ajaxcalls(config){
+    return new Promise(function(resolve,reject){
+        config
+        if (xhr.status == 200){
+            resolve(document.querySelector(".hidden").innerHTML = (xhr.response).value.joke);
+        }else{
+            reject(document.querySelector(".hidden").classList.add("error"));
+        }
+    }
+}
+}
+
+document.addEventListener("DOMContentLoaded",loadpage);
