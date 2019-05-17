18ab3451187e8bfe9c97e214061b16580cc66c82
BraianQ <50342216+BraianQ@users.noreply.github.com>
2019-05-17 14:26:19 -0300

    Finished exersice4 with somes errors

--- topic1/JavaScripts/scripts/main.js
+++ topic1/JavaScripts/scripts/main.js
@@ -2,11 +2,15 @@ function loadpage(){
 function hide() {
     document.querySelector(".hidden").classList.add("hide");
 }
-
-/*hide();*/
-/*-----------------------------------------------------------------------------------------------------*/
 /*
-function newalert(){
+let nodoBoton = document.querySelector('#button');
+nodoBoton.addEventListener('click',hide);
+*/
+
+
+/*-----------------------------------------------------------------------------------------------------*/
+
+function getDataAPI(){
     const xhr = new XMLHttpRequest();
     const url = 'http://api.icndb.com/jokes/random';
     let box;
@@ -22,16 +26,49 @@ function newalert(){
 
         }
     }
-}*/
+}
 
 let nodoBoton = document.querySelector('#button');
-nodoBoton.addEventListener('click',ajaxcalls);
+nodoBoton.addEventListener('click',getDataAPI);
 
 /*---------------------------------------------------------------------------------------------------*/
 
+let  searh = document.querySelector("#qsearch");
+searh.addEventListener('change',() => {
+    getDataAPIList(searh.val);
+})
+
+function getDataAPIList(q) {
+    let basicurl = 'https://api.github.com/search/repositories';
+    let parameter = '/' + encodeURIComponent(q);
+    let fullurl = basicurl + parameter;
+
+    console.log(fullurl);
+
+    let config2 = {url: fullurl, method: 'GET',responseType: 'json'};
+
+    ajaxCall(config2).then(function (response) {
+        let data = response.items;
+        console.log(data);
+        let list = document.getElementById('list');
+        for (const url of data){
+            let newli = document.createElement('li');
+            list.textContent = `${url.url}`
+            list.appendChild(newli);
+        }
+    }).catch(() => {
+      document.getElementById('list').innerHTML = 'error';
+    })
+}
+
+
+
+/*---------------------------------------------------------------------------------------------------*/
+
+
 let config1 = {url : 'http://api.icndb.com/jokes/random' , responseType : 'json' , method : 'GET'};
 
-function ajaxcalls(config){
+function ajaxCall(config1){
     return new Promise(function(resolve,reject){
         const xhr = new XMLHttpRequest();
         xhr.open(config1.method,config1.url);
@@ -39,14 +76,19 @@ function ajaxcalls(config){
         xhr.send('');
         xhr.onload = function (){
             if (xhr.status == 200){
-                let box = xhr.response;
-                resolve(document.querySelector(".hidden").innerHTML = box.value.joke);
+                resolve(xhr.response);
             }else{
-                reject(document.querySelector(".hidden").classList.add("error"));
+                reject('error');
         }
         }
     })
 }
+
+/*-------------------------------------------------------------------------------------------------------------------------*/ 
+
+
+
+
 }
 
 document.addEventListener("DOMContentLoaded",loadpage);
