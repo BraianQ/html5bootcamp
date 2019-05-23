465b0d92be35b565dfe56665b0b15b86c07966b9
BraianQ <50342216+BraianQ@users.noreply.github.com>
2019-05-23 09:14:08 -0300

    Finished exersice 4

--- topic2/exercise1/scripts/main.js
+++ topic2/exercise1/scripts/main.js
@@ -21,11 +21,16 @@ class EventEmitter{
 
     emit(Eventname){
         const event = this.events[Eventname];
-        console.log("AS");
+        console.log("entre");
         if (event){
             event.forEach(callback =>{
-                console.log("AS");
-                callback();
+                console.log(callback);
+                if (typeof callback === 'function') {
+                    callback();
+                  }
+                  else if (typeof callback === 'object') {
+                    callback.log(Eventname);
+                  }
             });
         }
     }
@@ -42,21 +47,51 @@ class Movie extends EventEmitter{
         this.title = title;
         this.year = year;
         this.duration = duration;
+        this.arrcast = [];
     }
 
     play(){
         console.log("The movie is playing");
+        this.emit("play");
     }
 
     pause(){
         console.log("The movie is pause");
+        this.emit("pause");
     }
 
     resume(){
         console.log("The movie is playing again");
+        this.emit("resume");
+    }
+
+    addCast(cast){
+        this.arrcast.push(cast);
+    }
+
+    showCast(){
+        console.log(this.arrcast);
     }
 }
 
+class Logger{
+    constructor(){
+    }
+
+    log(info){
+        console.log("The" + info + "Event has been emitted");
+    }
+}
+
+
+let social = {
+    share : function(friendName) {
+      console.log(`${friendName} shares ${this.title}`);
+    },
+    like : function(friendName) {
+      console.log(`${friendName} likes ${this.title}`);
+    }
+}
 
 function playEvent(){
     console.log("the movie is playing" + this.title);
@@ -70,9 +105,38 @@ function resumeEvent(){
     console.log("the movie is playing again");
 }
 
+
+
 let movie1 = new Movie("ADVENGERS: ENDGAME", 2019, 185);
 let movie2 = new Movie("ADVENGERS: INFINITY WAR", 2018, 160);
-let actor = new Actor("Robert Downey" , 54);
+let actorrobert = new Actor("Robert Downey" , 54);
+let actors = [
+    new Actor("Robert Downey1" , 54),
+    new Actor("Robert Downey2" , 54),
+    new Actor("Robert Downey3" , 54),
+];
+
+Object.assign(movie1, social);
+
+movie1.share.bind(movie1);
+movie1.like.bind(movie1);
+
+movie1.share("BLACK WIDOW");
+movie1.like("HULK");
+
+movie1.on("play",playEvent);
+
+let advengersListener = new Logger();
+
+movie1.on("play",advengersListener);
+
+movie1.play();
+
+
+movie1.addCast(actorrobert);
+movie1.showCast();
+movie2.addCast(actors);
+movie2.showCast();
 
 
 movie1.on("play" , playEvent);
